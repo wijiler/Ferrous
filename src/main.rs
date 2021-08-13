72acfd1ed3f007eb58062e0489582a01de1e437d
wijiler <josiahosgood@gmail.com>
2021-08-13 15:07:19 -0400

    why git

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 /// ferriclang's first line :)
-#[path = "./lexer/token.rs"]mod tokens;
 #[path = "./lexer/lexer.rs"]mod lexer;
+#[path = "./lexer/token.rs"]mod tokens;
 fn main() {
 println!("hi");
 }
