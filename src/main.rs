6a72ce9d2718ff06cc596e95b5ce64dc6b3f534a
wijiler <josiahosgood@gmail.com>
2021-08-13 15:34:36 -0400

    help thingy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
 /// ferriclang's first line :)
+use std::{env, process};
 #[path = "./lexer/lexer.rs"]mod lexer;
 #[path = "./lexer/token.rs"]mod tokens;
 fn main() {
-println!("hi");
+let args: Vec<String> = env::args().collect();
+if args.len() == 1 {
+    println!("ARGS not supplied");
+    process::exit(1);
+    }
+    match args[1].as_str() {
+    "-h" | "--help" => {
+        print_help();
+        process::exit(0);
+        }
+    _ => {}
+    }
 }
+
+fn print_help () {
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+println!("{}", 
+        format!(
+            r"ferric {}
+            
+            USAGE:
+            ferric [FILE] [ARGS]
+            
+            FLAGS:
+            -h show this help screen!
+
+            ARGS:
+            compile and run a .ferric file! or just compile (WIP)
+
+
+            ", VERSION
+            ) 
+        );
+
+}
+
