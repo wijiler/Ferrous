b56dfe4a3f42739fb6b89c9c2ad6df29e00e2e68
wijiler <josiahosgood@gmail.com>
2021-08-14 08:36:28 -0400

    tokens done will ad more

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,20 +1,5 @@
 #[path = "./token.rs"]pub mod tokens;
 
 fn main () {
-let strings = tokens::Tokens::Strings;
-let operator = tokens::Tokens::Operator;
-let int = tokens::Tokens::Int;
-let uint = tokens::Tokens::Uint;
-let float  = tokens::Tokens::Float;
-let lparen = tokens::Tokens::Lparen;
-let rparen = tokens::Tokens::Rparen; 
-let semi = tokens::Tokens::Semi;
-let rbrack = tokens::Tokens::Rbrack;
-let lbrack = tokens::Tokens::Lbrack;
-let rcbrack = tokens::Tokens::RCbrack;
-let lcbrack = tokens::Tokens::LCbrack;
-
-
-
 
 }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,18 +1,55 @@
-
+use std::fmt;
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Tokens {
-    Strings(String),
-    Operator(char),
-    Int(i64),
-    Uint(u64),
-    Float(f64),
+   // tokens
+    Plus,
     Lparen,
     Rparen,
+    Colon,
+    ColonColon,
     Semi,
     Rbrack,
     Lbrack,
     RCbrack,
     LCbrack,
+
+    // types
+    Int,
+    Float,
+    Bool,
+    True,
+    False,
+    Fn,
+    String,
+
 }
 
-pub fn main () { }
+impl Tokens {
+    pub fn value(&self) -> Option<&'static str> {
+        match self {
+        Self::Plus => Some("+"),
+        Self::Lparen => Some("("),
+        Self::Rparen => Some(")"),
+        Self::Colon => Some(":"),
+        Self::ColonColon => Some("::"),
+        Self::Semi => Some(";"),
+        Self::Rbrack => Some("]"),
+        Self::Lbrack => Some("["),
+        Self::RCbrack => Some("}"),
+        Self::LCbrack => Some("{"),
 
+        Self::Int => Some("int"),
+        Self::Float => Some("float"),
+        Self::Bool => Some("bool"),
+        Self::True => Some("true"),
+        Self::False => Some("false"),
+        Self::Fn => Some("func"),
+        Self::String => None,
+        }
+    }
+}
+impl fmt::Display for Tokens {
+fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "{}", self.value().unwrap_or(""))
+}
+}
