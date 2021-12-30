a892e3809baef1635f28548188291e185dd2642f
wijiler <josiahosgood@gmail.com>
2021-12-30 06:59:23 -0500

    working on lexer ... again

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,19 @@
+enum tokentype {
+    Id, // foo
+    Notation, // 1 + 1
+    Int, // 123
+    Literal, // "s" 
+    Char, // 's'
+    Function, // fn main(args here) { -_- code *-* } 
+    Return, // 
+}
+
+
+struct token {
+    
+}
+
+
 fn tokenize(file: String) {
    let wssfile = file.replace(" ","");
     for ct in wssfile.chars().next() {
@@ -5,6 +21,8 @@ fn tokenize(file: String) {
             
         } 
     }
+
 }
-    
+
+        
 
