fb4600f48f0655d3b8f486b6a59f5c623ef0a775
wijiler <josiahosgood@gmail.com>
2021-12-31 07:20:17 -0500

    takin a break still working on lexer

--- src/token.rs
+++ src/token.rs
@@ -8,20 +8,19 @@ enum tokentype {
     Function, // fn main(args here) { -_- code *-* }
     Return,   // return duh :p
 }
-enum token {
-    Ttype(tokentype),
-    Structure{}, // depends on the token type see Docs/tokenization.md
+
+enum structures {
+    Id{
+        
+    },
+}
+
+struct token {
+    ttype: tokentype,
+    structure:, // depends on the token type see Docs/tokenization.md
 }
 impl token {
     fn new(tt : tokentype) -> token {
-        match tt {
-            tokentype::Id => token {
-                Ttype(tokentype::Id),
-                Structure{
-                    Name:String,
-                }
-            }
-        }
     }
 }
 fn tokenize(file: String) {
