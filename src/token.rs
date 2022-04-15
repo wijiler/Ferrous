d1e20485dff6c313a81c2432ebdcf8fc3e086e4f
wijiler <josiahosgood@gmail.com>
2022-04-15 15:11:45 -0400

    tokens and stuff but I think I can actually make somethign that will work now

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,4 @@
-use std::fs;
+/*use std::fs;
 pub fn tokenize(filename:&String) {
     let mut contents = fs::read_to_string(filename)
         .expect("Something went wrong reading please try again");
@@ -10,4 +10,67 @@ pub fn tokenize(filename:&String) {
     }
     contents.retain(|c| !c.is_whitespace());
    println!("File contents:\n{}",contents);
+}*/
+struct token {
+    value:char,
+}
+/*
+        TOKEN_ADD,
+        TOKEN_SUBTRACT,
+        TOKEN_EQUALS,
+        TOKEN_BANG,
+        TOKEN_MODULO,
+        TOKEN_DIVIDE,
+        TOKEN_MULTIPLY,
+        TOKEN_SEMI,
+        TOKEN_LPAREN,
+        TOKEN_RPAREN,
+        TOKEN_COMMA,
+        TOKEN_COLON,
+*/
+impl token {
+ fn new(v:char) -> Self {
+    Self {value:v,}
+ }
+ // o for operator etc. etc.
+ fn o_equal() {
+    token::new('=');
+ }
+
+ fn o_minus() {
+    token::new('-');
+ }
+
+ fn o_add() {
+    token::new('+');
+ }
+
+ fn o_divide() {
+    token::new('/');
+ }
+
+ fn o_multiply() {
+    token::new('*');
+ }
+
+ fn t_lparen() {
+    token::new('(');
+ }
+
+ fn t_rparen() {
+    token::new(')');
+ }
+
+ fn s_comma() {
+    token::new(':');
+ }
+
+ fn s_colon() {
+    token::new(';');
+ }
+
+ fn s_semi() {
+    token::new(';');
+ }
+
 }
