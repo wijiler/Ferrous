dc0c835340d2996f4ad696dfb6c080b1fbd5c6b1
wijiler <josiahosgood@gmail.com>
2022-06-01 05:56:43 -0400

    more work on the parser

--- src/parser.rs
+++ src/parser.rs
@@ -22,9 +22,34 @@ fn error(e:Errort,line:usize) {
         }
     }
 }
+enum Value {
+    string(String),
+    int(u64),
+    float(f64),
+}
+enum Registers64{
+    // temp/return
+    rax{value:Value},
+    // calee saved
+    rbx{value:Value},
+    /// rcx rdx rsi rdi r8 r9 are all used to pass function arguments so I will just comment the
+    /// number argument they put in
+    rcx{value:Value}, // 4
+    rdx{value:Value}, // 3
+    rsp{value:Value}, // stack pointer
+    rbp{value:Value}, // callee saved ; base pointer
+    rsi{value:Value}, // 2
+    rdi{value:Value}, // 1
+    r8{value:Value}, // 5
+    r9{value:Value}, // 6
+    r10{value:Value}, // temp
+    r11{value:Value},// temp
+}
+
+//enum Registers32{}
 
 enum AsmInstructions {
-    movl,
-    movq,
-    Ascii,
+  //movl{value:i32,register:Registers32},// this is a 64 bit lang I dont want to work on 32 bit rn
+    movq{value:Value,register:Registers64},// 64 bit mov
+    ascii{value:String},
 }
