42d02f5f87b89478938e6d66c7cd2c171d6685e2
wijiler <josiahosgood@gmail.com>
2022-08-03 11:31:20 -0400

    parser work

--- src/ast/util.rs
+++ src/ast/util.rs
@@ -1,4 +1,5 @@
 use token::TokenType;
+use std::fmt;
 #[derive(Debug,Clone)]
 pub struct Parser {
    tokens:Vec<TokenType>, 
@@ -16,4 +17,25 @@ impl Parser {
     pub fn nom (self) -> TokenType { // its the eat/consume function but I think its funnier to call it nom
         self.tokens.to_vec().into_iter().next().unwrap()
     }
+}
+#[derive(Debug,Clone)]
+pub struct Astnode {
+    value:String,     
+    children:Vec<Astnode>,
+}
+pub struct Ast {
+    tree:Vec<Astnode>
+}
+impl fmt::Display for Ast {
+     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+       for node in self.tree.to_vec(){
+           writeln!(f,"{}", node.value);
+           for child in node.children {
+               if node.children.to_vec().len() == 1 {
+               }
+           }
+           //writeln!(f,"{}",format!("{}",node.value)) 
+       }
+       Ok(())  
+     }
 }
